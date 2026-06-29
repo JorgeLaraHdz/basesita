1896ed556e8820b01d6a419d723bc88e9d15d491
JorgeDeLaraH <juan.idiota.deloeraloera@gmail.com>
2026-06-29 12:12:10 -0600

    añadi un print

--- src/main/java/com/mycompany/basesita/Basesita.java
+++ src/main/java/com/mycompany/basesita/Basesita.java
@@ -12,6 +12,7 @@ import java.sql.SQLException;
 public class Basesita {
 
     public static void main(String[] args) {
+        System.out.println("Holis");
         
         try (Connection conexion = Conexion.conectar()) {
 
